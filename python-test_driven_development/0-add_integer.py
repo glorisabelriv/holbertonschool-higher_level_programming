c0db2462e7a3bc0eac64d6b36295b76dc43c3dbd
glorisabelriv <glorisabel200@gmail.com>
2024-05-20 21:59:58 -0400

    Write a function that adds 2 integers.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,24 +8,6 @@ a and b must be first casted to integers if they are float
 
 
 def add_integer(a, b=98):
-    """
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    >>> add_integer(100.3, -2)
-    98
-    >>> add_integer(0, 0)
-    0
-    >>> add_integer(3, 5)
-    8
-    >>> add_integer(5, 5)
-    10
-    >>> add_integer(120, -20)
-    100
-    >>> add_integer(200, -100)
-    100
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
