328418601797bba78fd7cec51a6c0e76d970ca94
glorisabelriv <glorisabel200@gmail.com>
2024-05-22 21:04:21 -0400

    Write a function that returns the list of available attributes and methods of an object:

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that returns the list of available
+attributes and methods for an object
+"""
+
+
+def lookup(obj):
+    """
+Function that returns the list of available
+attributes and methods for an object
+"""
+    return dir(obj)
