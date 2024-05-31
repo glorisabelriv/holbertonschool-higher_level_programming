8638d4526c47417d30ae6e4d1ff7986cf5936fe8
glorisabelriv <glorisabel200@gmail.com>
2024-05-31 12:40:04 -0400

    Write a basic Python script to fetch posts from JSONPlaceholder using requests.get().

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,27 +5,26 @@ def fetch_and_print_posts():
     # Hacer la solicitud GET a JSONPlaceholder
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
-    # Imprimir el codigo de estado de la respuesta
-    print(f"Codigo de Estado: {response.status_code}")
+    # Imprimir el código de estado de la respuesta
+    print(f"Status Code: {response.status_code}")
 
     # Si la solicitud fue exitosa, analizar y mostrar los datos
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
-            print(post['tittle'])
+            print(post['title'])
 
 def fetch_and_save_posts():
     # Hacer la solicitud GET a JSONPlaceholder
-    response = requests.get('https://jsonplaceholder.typicode.com/post')
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     # Si la solicitud fue exitosa, analizar y guardar los datos
     if response.status_code == 200:
         posts = response.json()
-        data_to_save = [{'id': post['id'], 'tittle': post['tittle'], 'body': post['body']} for post in posts]
+        data_to_save = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
 
         # Guardar los datos en un archivo CSV
-        import csv
         with open('posts.csv', mode='w', newline='') as file:
-            writer = csv.DictWriter(file, filenames=['id', 'title', 'body'])
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
             writer.writeheader()
             writer.writerows(data_to_save)
