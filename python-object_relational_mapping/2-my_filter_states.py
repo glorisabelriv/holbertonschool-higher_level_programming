7dd4015ee17b8a603195e628efc8fa1fc2c6a26e
glorisabelriv <glorisabel200@gmail.com>
2024-06-20 10:27:11 -0400

    Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
-""" Script that takes in an argument and displays
-all values in the states table """
-
+"""script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa
+where name matches the argument."""
 import MySQLdb
 from sys import argv
-import sys
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+        )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
+    query = "SELECT * FROM states WHERE\
+        BINARY name LIKE '{}'".format(state_name)
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-        cursor.close()
-        db.close()
+        print(row)
+
+    cursor.close()
+    db.close()
