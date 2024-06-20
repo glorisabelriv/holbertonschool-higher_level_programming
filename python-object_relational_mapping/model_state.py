fd4b48903e08ffccddf8e52b5018b9842e4fb8f7
glorisabelriv <glorisabel200@gmail.com>
2024-06-20 09:48:43 -0400

    Write a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa:

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module provide a class that map to State table"""
+# Module for Connecting To MySQL database
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''Class mapping table Sate'''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
