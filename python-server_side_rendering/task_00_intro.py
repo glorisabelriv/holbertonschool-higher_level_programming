9e0e9b81071078e5724a153f7b778b180e394e49
glorisabelriv <glorisabel200@gmail.com>
2024-07-15 10:15:13 -0400

    Error un missing data

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -26,8 +26,10 @@ def generate_invitations(template, attendees):
         # Reemplazar los placeholders del template
         invitation = template
         for key in ["name", "event_title", "event_date", "event_location"]:
-            value = attendee.get(key, "Failed")
-            invitation = invitation.replace(f"{{{{{key}}}}}", value if value else "Failed")
+            value = attendee.get(key, "N/A")
+            if value is None:
+                value = "N/A"
+            invitation = invitation.replace(f"{{{{{key}}}}}", value)
 
 
             # Nombre del archivo de salida
